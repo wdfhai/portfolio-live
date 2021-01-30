e97d0d9ecfee55b9ffc859398481abb28f2b6f1a
wdfhai <wd.fhai@outlook.com>
2021-01-30 00:18:23 -0600

    changed extra stuff section to Pinned section, fixed Suitcase link to my own repo

--- script/script.js
+++ script/script.js
@@ -1,55 +1,55 @@
 $("#buttonAboutMe").on('click', runAboutMe)
 $("#buttonConnectWithMe").on('click', runConnectWithMe)
 $("#buttonViewMyPortfolio").on('click', runViewMyPortfolio)
-$("#buttonExtraStuff").on('click', runExtraStuff)
+$("#buttonViewMyBest").on('click', runMyBest)
 $("#submitButton").on('click', runContactForm)
 
 function runAboutMe (){
     $("#buttonAboutMe").addClass('pressed');
     $("#buttonConnectWithMe").removeClass('pressed');
     $("#buttonViewMyPortfolio").removeClass('pressed');
-    $("#buttonExtraStuff").removeClass('pressed');
+    $("#buttonViewMyBest").removeClass('pressed');
     $("#divWelcome").attr('style', 'display: none');
     $("#divAboutMe").attr('style', 'display: block');
     $("#divConnectWithMe").attr('style', 'display: none');
     $("#divViewMyPortfolio").attr('style', 'display: none');
-    $("#divExtraStuff").attr('style', 'display: none');
+    $("#divViewMyBest").attr('style', 'display: none');
 };
 
 function runConnectWithMe (){
     $("#buttonAboutMe").removeClass('pressed');
     $("#buttonConnectWithMe").addClass('pressed');
     $("#buttonViewMyPortfolio").removeClass('pressed');
-    $("#buttonExtraStuff").removeClass('pressed');
+    $("#buttonViewMyBest").removeClass('pressed');
     $("#divWelcome").attr('style', 'display: none');
     $("#divAboutMe").attr('style', 'display: none');
     $("#divConnectWithMe").attr('style', 'display: block');
     $("#divViewMyPortfolio").attr('style', 'display: none');
-    $("#divExtraStuff").attr('style', 'display: none');
+    $("#divViewMyBest").attr('style', 'display: none');
 };
 
 function runViewMyPortfolio (){
     $("#buttonAboutMe").removeClass('pressed');
     $("#buttonConnectWithMe").removeClass('pressed');
     $("#buttonViewMyPortfolio").addClass('pressed');
-    $("#buttonExtraStuff").removeClass('pressed');
+    $("#buttonViewMyBest").removeClass('pressed');
     $("#divWelcome").attr('style', 'display: none');
     $("#divAboutMe").attr('style', 'display: none');
     $("#divConnectWithMe").attr('style', 'display: none');
     $("#divViewMyPortfolio").attr('style', 'display: block');
-    $("#divExtraStuff").attr('style', 'display: none');
+    $("#divViewMyBest").attr('style', 'display: none');
 };
 
-function runExtraStuff (){
+function runMyBest (){
     $("#buttonAboutMe").removeClass('pressed');
     $("#buttonConnectWithMe").removeClass('pressed');
     $("#buttonViewMyPortfolio").removeClass('pressed');
-    $("#buttonExtraStuff").addClass('pressed');
+    $("#buttonViewMyBest").addClass('pressed');
     $("#divWelcome").attr('style', 'display: none');
     $("#divAboutMe").attr('style', 'display: none');
     $("#divConnectWithMe").attr('style', 'display: none');
     $("#divViewMyPortfolio").attr('style', 'display: none');
-    $("#divExtraStuff").attr('style', 'display: block');
+    $("#divViewMyBest").attr('style', 'display: block');
 };
 
 function runContactForm (event) {
